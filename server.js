8ef962c591652905883c1ab916f79b662af44e7d
rayvenjm <jeremiahjmesa@gmail.com>
2021-09-06 18:26:34 +0800

    Redux Implementation

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Start with nodemon server
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 var dbconnection = require('./db')
+var productsRoute = require('./routes/productsRoute')
+
+app.use(bodyParser.json())
+app.use('/api/products' , productsRoute)
 
 app.get("/",(req,res)=>{
     res.send("This is from the backend")
