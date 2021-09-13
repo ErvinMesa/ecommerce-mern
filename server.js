d0480ead26b3028f7dde0b40e043afbd49c6ce61
rayvenjm <jeremiahjmesa@gmail.com>
2021-09-13 17:52:35 +0800

    Authentication Chapter Complete

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 var dbconnection = require('./db')
 var productsRoute = require('./routes/productsRoute')
-
+var userRoute = require("./routes/userRoute")
 app.use(bodyParser.json())
-app.use('/api/products' , productsRoute)
-
+app.use('/api/products/' , productsRoute)
+app.use('/api/users/',userRoute)
 app.get("/",(req,res)=>{
     res.send("This is from the backend")
 })
